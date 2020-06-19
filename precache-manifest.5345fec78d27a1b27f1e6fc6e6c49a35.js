b7ff56d91fc862b79cf00b832f78b4f054f76cc6
codecakes <codecakes@users.noreply.github.com>
2020-06-19 15:11:17 +0000

    deploy: cfda96dd747ec4ceec74b9b493707deb06253911

---
+++ precache-manifest.5345fec78d27a1b27f1e6fc6e6c49a35.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "566eb32f96eacd12bac1055f38938d0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "209c2cbed1b3299e1567",
+    "url": "/static/css/main.b1b2520a.chunk.css"
+  },
+  {
+    "revision": "8dabd0290852421166a4",
+    "url": "/static/js/2.9f7b12a5.chunk.js"
+  },
+  {
+    "revision": "fea0fe9c8611d72dae97bba51bde9871",
+    "url": "/static/js/2.9f7b12a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "209c2cbed1b3299e1567",
+    "url": "/static/js/main.944372e9.chunk.js"
+  },
+  {
+    "revision": "d3be38e7f455fb7c073c",
+    "url": "/static/js/runtime-main.25917a94.js"
+  },
+  {
+    "revision": "c5590569aab553af03722baa7d379978",
+    "url": "/static/media/loading.c5590569.svg"
+  },
+  {
+    "revision": "3c65b6d050f1a718e6cd75cf3a9cab19",
+    "url": "/static/media/register-banner.3c65b6d0.jpg"
+  }
+]);
--- src/reducers/index.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from 'redux';
-import projects from './projects';
-export default combineReducers({
-    projects,
-});
