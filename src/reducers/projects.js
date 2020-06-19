ca48074d91b5e9238c5da306b408fd99e0b264db
joey <jabreukelen@gmail.com>
2020-06-19 11:24:48 +0200

    update PROJECTS_KEY so old incompatable data won't be loaded

--- src/reducers/projects.js
+++ src/reducers/projects.js
@@ -2,7 +2,7 @@ import { ADD_PROJECT } from "../actions/projects";
 
 const toObj = v => v && JSON.parse(v);
 const toStr = v => v && JSON.stringify(v);
-const PROJECTS_KEY = 'projects__00001';
+const PROJECTS_KEY = 'projects__00002';
 const getProjects = () => toObj(localStorage.getItem(PROJECTS_KEY)) || [];
 const setProjects = state => localStorage.setItem(PROJECTS_KEY, toStr(state)) || state;
 const projects = (state = getProjects(), action) => {
