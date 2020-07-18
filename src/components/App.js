227ba77d9f6d8cbb695445ec856108f85ba3fdb2
codecakes <akulmat@protonmail.com>
2020-07-18 23:02:02 +0530

    Landing site works. TODO: Fix this path to work on custom route:

--- src/components/App.js
+++ src/components/App.js
@@ -22,10 +22,14 @@ const PrivateRoute = ({ dispatch, component, ...rest }) => {
   );
 };
 
+/**
+  TODO(@joeydebreuk): Fix this path to work on custom route!
+*/
 const App = () =>
   <Router>
     <Switch>
-      <Route path="/" exact render={() => <Redirect to="/app/dashboard" />} />
+      <Route path='/' component={() => { window.location = '/website/home.html'; return null;} }/>
+      <Route path="/xcov-demo" exact render={() => <Redirect to="/app/dashboard" />} />
       <PrivateRoute path="/app" component={Layout} />
       <Route path="/login" component={Login} />
       <Route path="/register" component={Register} />
